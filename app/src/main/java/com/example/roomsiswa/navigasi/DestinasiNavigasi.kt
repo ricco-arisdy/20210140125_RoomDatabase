e9fc1560fa3bf09f27c8e5c7643f6ba49d8ec71e
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-05 21:27:09 +0700

    add  val route: String and val titleRes: Int

--- app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,13 @@
 package com.example.roomsiswa.navigasi
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman
+     */
+    val titleRes: Int
 }
