4cf24edff6c699fbe640804f6dc4f4358c0d938a
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-05 21:44:58 +0700

    add  fun EntrySiswaScreen

--- app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
@@ -1,2 +1,19 @@
 package com.example.roomsiswa.ui.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.model.EntryViewModel
+import com.example.roomsiswa.model.PenyediaViewModel
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory= PenyediaViewModel.Factory)
+){
+
+}
