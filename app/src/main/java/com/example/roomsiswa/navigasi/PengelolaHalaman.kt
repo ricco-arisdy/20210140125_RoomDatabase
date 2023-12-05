b997f31aee84abf046131202954f026b3ea371f5
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-06 01:46:49 +0700

    add fun HostNavigasi and fun SiswaApp

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -11,8 +11,21 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.Halaman.DestinasiEntry
+import com.example.roomsiswa.ui.Halaman.DestinasiHome
+import com.example.roomsiswa.ui.Halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.Halaman.HomeScreen
 
+
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
@@ -36,3 +49,19 @@ fun SiswaTopAppBar(
         )
 }
 
+@Composable
+fun HostNavigasi(
+    navController: NavHostController, modifier: Modifier = Modifier
+) {
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
+}
