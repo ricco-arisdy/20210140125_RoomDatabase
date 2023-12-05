ae4304e6920ffdc8cdeeef7a52439cae3370e378
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-05 21:15:26 +0700

    add object penyediaviewmodel and creationextras aplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.roomsiswa.AplikasiSiswa
+
+
+object PenyediaViewModel{
+    fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
+}
