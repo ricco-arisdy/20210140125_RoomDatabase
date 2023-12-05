3f9ac4978852c1a32221681c9c7266ab166925c5
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-05 20:56:45 +0700

    add data class in homeviemodel class

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.roomsiswa.model
 
+import com.example.roomsiswa.data.Siswa
+
 class HomeViewModel {
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
 }
