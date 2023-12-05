d45a72d6c11930d832c0557f1bc28f29fbf58c13
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-05 21:21:06 +0700

    add val Factory inobject penyediaviewmodel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -2,10 +2,24 @@ package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomsiswa.AplikasiSiswa
 
 
 object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+    /**
+     * Fungsi ektensi query untuk objek [Aplication] dan mengembalikan sebuah instance dari
+     * [AplikasiSiswa]
+     */
     fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 }
